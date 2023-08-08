6bf8aad05df09a81b7b5389aeb55fd772675533e
edwynn <edwynn.sulbaran@andeanwide.com>
2023-08-08 15:56:44 -0400

    update file test

--- functionTest2.js
+++ functionTest2.js
@@ -1,3 +1,6 @@
-function test2() {
+function test2(event) {
+  console.log(event.target.dataset["clientid"]);
+  console.log(event.target.dataset["environment"]);
+  console.log(event.target.dataset["metadata"]);
   alert("test2 execute");
 }
