94415928d3028c6550673f23620c45ee7da680c4
GaaCBot <noreply@gaac.vercel.app>
2023-08-08 16:20:14 -0400

    Uploaded functionTest2.js via GAAC

--- uploads/functionTest2.js
+++ uploads/functionTest2.js
@@ -1,6 +1,13 @@
-function test2(event) {
-  console.log(event.target.dataset["clientid"]);
-  console.log(event.target.dataset["environment"]);
-  console.log(event.target.dataset["metadata"]);
+function test2() {
+  // console.log(event.target.dataset["clientid"]);
+  // console.log(event.target.dataset["environment"]);
+  // console.log(event.target.dataset["metadata"]);
+
+  const attr = document.getElementById("runner-button");
+  console.log(attr);
+  console.log(attr?.dataset.metadata);
+  console.log(attr?.dataset.clientid);
+  console.log(attr?.dataset.environment);
+
   alert("test2 execute");
 }
