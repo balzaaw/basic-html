2b1c2de4af13411a1c0dbdebb66318aa0ef55d8e
edwynn <edwynn.sulbaran@andeanwide.com>
2023-08-10 11:42:21 -0400

    update style

--- runner-button.js
+++ runner-button.js
@@ -1,8 +1,4 @@
 function getAttr() {
-  // console.log(event.target.dataset["clientid"]);
-  // console.log(event.target.dataset["environment"]);
-  // console.log(event.target.dataset["metadata"]);
-
   const attr = document.getElementById("runner-button");
   console.log(attr);
   console.log(attr?.dataset.metadata);
@@ -15,6 +11,20 @@ function getAttr() {
 const addEventButton = () => {
   const button = document.getElementById("runner-button");
   const imageSource = button.dataset.img;
+  const buttonStyle = button.dataset.style;
+
+  if (buttonStyle) {
+    button.style.cssText = buttonStyle;
+  } else {
+    button.style.backgroundColor = "background-color:#10528d";
+    button.style.color = "white";
+    button.style.fontSize = "14";
+    button.style.fontWeight = "700";
+    button.style.textAlign = "center";
+    button.style.display = "flex";
+    button.style.flexDirection = "row-reverse";
+    button.style.rowGap = "10px";
+  }
 
   if (imageSource) {
     const img = document.createElement("img");
