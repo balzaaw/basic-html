102518c2d1cc64f33b167e7a43b3a33e85b8f990
edwynn <edwynn.sulbaran@andeanwide.com>
2023-08-10 13:54:46 -0400

    update js file

--- runner-button.js
+++ runner-button.js
@@ -8,33 +8,52 @@ function getAttr() {
   alert("getAttr execute");
 }
 
+function setDefaultStylesButton(button) {
+  button.style.display = "flex";
+  button.style.flexDirection = "row-reverse";
+  button.style.justifyContent = "space-evenly";
+  button.style.gap = "5px";
+  button.style.cursor = "pointer";
+  button.style.borderRadius = "8px";
+  button.style.fontSize = "1em";
+}
+
+function setStyleButton(button, styles) {
+  if (styles) {
+    button.style.cssText += buttonStyle;
+  } else {
+    button.style.backgroundColor = "#10528d";
+    button.style.color = "white";
+    button.style.fontWeight = "700";
+    button.style.textAlign = "center";
+    button.style.transition = "border-color 0.25s";
+  }
+}
+
+function setLogoButton(imgSource) {
+  if (imgSource) {
+    const img = document.createElement("img");
+    img.style.width = "32px";
+    img.style.height = "32px";
+    img.style.marginLeft = "5px";
+    img.style.marginRight = "5px";
+    img.style.objectFit = "contain";
+
+    img.setAttribute("src", imgSource);
+    button.appendChild(img);
+  }
+}
+
 const addEventButton = () => {
   const button = document.getElementById("runner-button");
   const imageSource = button.dataset.img;
   const buttonStyle = button.dataset.style;
 
-  if (buttonStyle) {
-    button.style.cssText = buttonStyle;
-  } else {
-    button.style.backgroundColor = "background-color:#10528d";
-    button.style.color = "white";
-    button.style.fontSize = "14";
-    button.style.fontWeight = "700";
-    button.style.textAlign = "center";
-    button.style.display = "flex";
-    button.style.flexDirection = "row-reverse";
-    button.style.rowGap = "10px";
-  }
-
-  if (imageSource) {
-    const img = document.createElement("img");
-    img.style.width = "32px";
-    img.style.height = "32px";
-
-    img.setAttribute("src", imageSource);
-    button.appendChild(img);
-  }
+  setDefaultStylesButton(button);
+  setStyleButton(button, buttonStyle);
+  setLogoButton(imageSource);
 
   button.addEventListener("click", getAttr);
 };
+
 window.onload = addEventButton;
