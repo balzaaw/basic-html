fbba18b1cf62262491212f5e78889fb3e20ea7cb
edwynn <edwynn.sulbaran@andeanwide.com>
2023-08-22 16:37:14 -0400

    update runner file

--- runner-button.js
+++ runner-button.js
@@ -1,75 +1,75 @@
-function getAttr() {
-  const attr = document.getElementById("runner-button");
-  console.log(attr);
-  console.log(attr?.dataset.metadata);
-  console.log(attr?.dataset.clientid);
-  console.log(attr?.dataset.environment);
+const runner = {
+  getAttr: async function () {
+    // const attr = document.getElementById("runner-button");
+    // console.log(attr);
+    // console.log(attr?.dataset.metadata);
+    // console.log(attr?.dataset.clientid);
+    // console.log(attr?.dataset.environment);
 
-  alert("getAttr execute");
-}
+    const data = await fetch("https://api.nationalize.io/?name=nathaniel");
+    console.log({ data });
+    alert("getAttr execute");
+  },
+  setDefaultStylesButton: function (button) {
+    button.style.padding = "0.6em 1.2em";
+    button.style.display = "flex";
+    button.style.flexDirection = "row-reverse";
+    button.style.justifyContent = "space-evenly";
+    button.style.alignItems = "center";
+    button.style.gap = "5px";
+    button.style.cursor = "pointer";
+    button.style.borderRadius = "8px";
+    button.style.fontSize = "1em";
+  },
+  setStyleButton: function (button, styles) {
+    if (styles) {
+      button.style.cssText += styles;
+    } else {
+      button.style.backgroundColor = "#10528d";
+      button.style.color = "white";
+      button.style.fontWeight = "600";
+      button.style.textAlign = "center";
+      button.style.transition = "border-color 0.25s";
+    }
+  },
+  setLogoButton: function (button, imgSource) {
+    if (imgSource) {
+      const img = document.createElement("img");
+      img.style.width = "32px";
+      img.style.height = "32px";
+      img.style.marginLeft = "5px";
+      img.style.marginRight = "5px";
+      img.style.objectFit = "contain";
 
-function setDefaultStylesButton(button) {
-  button.style.padding = "0.6em 1.2em";
-  button.style.display = "flex";
-  button.style.flexDirection = "row-reverse";
-  button.style.justifyContent = "space-evenly";
-  button.style.alignItems = "center";
-  button.style.gap = "5px";
-  button.style.cursor = "pointer";
-  button.style.borderRadius = "8px";
-  button.style.fontSize = "1em";
-}
+      img.setAttribute("src", imgSource);
+      button.appendChild(img);
+    }
+  },
+  getAttrFromNode: function () {
+    const button = document.getElementById("runner-button");
+    const imageSource = button.dataset.img;
+    const buttonStyle = button.dataset.style;
+    const ignoreStyle = button.dataset.ignorestyle;
 
-function setStyleButton(button, styles) {
-  if (styles) {
-    button.style.cssText += styles;
-  } else {
-    button.style.backgroundColor = "#10528d";
-    button.style.color = "white";
-    button.style.fontWeight = "600";
-    button.style.textAlign = "center";
-    button.style.transition = "border-color 0.25s";
-  }
-}
+    return {
+      imageSource,
+      buttonStyle,
+      ignoreStyle,
+      button,
+    };
+  },
+  addEventButton: function () {
+    const { button, buttonStyle, ignoreStyle, imageSource } =
+      this.getAttrFromNode();
 
-function setLogoButton(button, imgSource) {
-  if (imgSource) {
-    const img = document.createElement("img");
-    img.style.width = "32px";
-    img.style.height = "32px";
-    img.style.marginLeft = "5px";
-    img.style.marginRight = "5px";
-    img.style.objectFit = "contain";
+    if (!ignoreStyle) {
+      this.setDefaultStylesButton(button);
+      this.setStyleButton(button, buttonStyle);
+    }
 
-    img.setAttribute("src", imgSource);
-    button.appendChild(img);
-  }
-}
-
-function getAttrFromNode() {
-  const button = document.getElementById("runner-button");
-  const imageSource = button.dataset.img;
-  const buttonStyle = button.dataset.style;
-  const ignoreStyle = button.dataset.ignorestyle;
-
-  return {
-    imageSource,
-    buttonStyle,
-    ignoreStyle,
-    button,
-  };
-}
-
-const addEventButton = () => {
-  const { button, buttonStyle, ignoreStyle, imageSource } = getAttrFromNode();
-
-  if (!ignoreStyle) {
-    setDefaultStylesButton(button);
-    setStyleButton(button, buttonStyle);
-  }
-
-  setLogoButton(button, imageSource);
-  button.addEventListener("click", getAttr);
+    this.setLogoButton(button, imageSource);
+    button.addEventListener("click", getAttr);
+  },
 };
 
-window.onload = addEventButton;
+window.onload = runner.addEventButton();
